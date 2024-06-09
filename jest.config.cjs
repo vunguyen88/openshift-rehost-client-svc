0301a0a401c4d5ca8f8846bad29640598654f114
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-06-09 15:45:03 -0400

    update jest config for coverage file output and pipeline

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,5 +6,8 @@ module.exports = {
     '^.+\\.(jpg|jpeg|png|gif|webp|svg|css)$': 'jest-transform-stub'
   },
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/"]
 };
